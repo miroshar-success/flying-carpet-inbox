9d1efa08a313687e4d1ea47e67ea62f0bd0fae67
dancerking <dancerking306@gmail.com>
2022-07-16 00:39:09 +0900

    added connection info

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,8 +1,2 @@
-module.exports = {
-  mongoURI: "mongodb://localhost:27017/whatsapp", // Insert your MongoDB connection URI here
-  /* Example DB connection 
-    "mongodb://user_name:password@ds153752.mlab.com:53752/devconnector"
-    
-    */
-  secretOrKey: "whatsapp" // Add your secret string or key here
-};
+const datas = require("./data.json");
+module.exports = datas;
